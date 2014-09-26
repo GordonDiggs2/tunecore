2c840c327545ab6ba058f36fecc21a52353d53d2
GordonDiggs <gordon@gordondiggs.com>
2014-09-25 21:49:56 -0400

    Add more tests for artist

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe Artist, :type => :model do
   it "should require a name" do
     artist = Artist.new
-    expect(artist.valid?).to eq(false)
+    expect(artist).not_to be_valid
     expect(artist.errors[:name]).to eq(["can't be blank"])
+  end
+
+  it "should have many albums" do
+    expect(Artist.reflect_on_association(:albums).macro).to eq(:has_many)
   end
 end
