8d9bed334b701fa354a49527d9a3ea8186e4ddd0
GordonDiggs <gordon@gordondiggs.com>
2014-09-26 20:23:22 -0400

    Make the seeds file match spec

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@ end
 
 puts "Creating seed data"
 
+# Create 10 artists, each with 10 albums, each with 5 songs.
+# Each run makes 10 artists, 100 albums and 500 songs.
 Artist.create(10.times.map { {name: random_name} }).each do |artist|
   Album.create(10.times.map { {title: random_name, year: (1960..2014).to_a.sample, artist_id: artist.id} }).each do |album|
-    Song.create(12.times.map { {album_id: album.id, title: random_name} })
+    Song.create(5.times.map { {album_id: album.id, title: random_name} })
   end
 end
 
